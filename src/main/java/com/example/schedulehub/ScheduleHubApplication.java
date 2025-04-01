6b2d1739fe7d77e6af8b9aa74ae05ef8c7bb7b89
janghwal <jhp337037@gmail.com>
2025-04-01 13:33:39 +0900

    chore: ScheduleHub 초기 설정
    - 프로젝트 기본 구조 생성

---
+++ src/main/java/com/example/schedulehub/ScheduleHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulehub/ScheduleHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
