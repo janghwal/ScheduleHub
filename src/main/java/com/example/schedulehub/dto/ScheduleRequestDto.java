43cd6344c21bd2732459c9ec3948d8d242b05d99
janghwal <jhp337037@gmail.com>
2025-04-01 14:19:24 +0900

    feat: ScheduleRequestDto 및 ScheduleResponseDto 구현
    - ScheduleRequestDto에 이름 제목 내용 필드 추가
    - ScheduleResponseDto에 이름 제목 내용 필드 추가

--- src/main/java/com/example/schedulehub/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/schedulehub/dto/ScheduleRequestDto.java
@@ -1,4 +1,15 @@
 package com.example.schedulehub.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ScheduleRequestDto {
+
+    private String userName;
+
+    private String title;
+
+    private String contents;
 }
